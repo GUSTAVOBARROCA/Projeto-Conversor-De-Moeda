f6662d90c93fd9f1a8b8939c1f8d2275ccd4814d
Gustavo <gustavobarroca@hotmail.com>
2023-03-07 20:08:14 -0300

    adicionado a moeda bitcoin

--- assets/sripts.js
+++ assets/sripts.js
@@ -2,7 +2,8 @@ const button = document.getElementById('convert-button')
 const select = document.getElementById('currency-select')
 
 const dolar = 5.2
-const euro = 5.9
+const euro = 5.5
+const bitCoin = 114.000
 
 const convertValues = () => {
     const inputReais = document.getElementById('input-real').value
@@ -30,6 +31,14 @@ const convertValues = () => {
     
         }).format(inputReais / euro)
     }
+
+    if(select.value === 'Bitcoin') {
+        currencyValueText.innerHTML = Intl.NumberFormat("de-DE", {
+            // style:'currency', 
+            currency:'BTC' ,           
+    
+        }).format(inputReais / bitCoin.toFixed)
+    }
     // currencyValueText.innerHTML = inputReais / dolar
 
     
@@ -44,12 +53,16 @@ const changeCurrency = () => {
 
     if(select.value === 'US$ Dolár americano') {
         currencyName.innerHTML = "Dolár americano"
-        currencyImg.src = "img/estados-unidos (1) 1.svg"
+        currencyImg.src = "img/estados-unidos (1) 1.png"
      }
 
     if(select.value === '€ Euro') {
         currencyName.innerHTML = "Euro"
         currencyImg.src = "img/Design-sem-nome-1.png"
+     }
+     if(select.value === 'Bitcoin') {
+        currencyName.innerHTML = "Bitcoin"
+        currencyImg.src = "img/Design sem nome (1) 1.png"
      }
     
      convertValues()
